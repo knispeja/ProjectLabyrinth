19be49583e85545b957f00920fafeae9888d34f8
Jacob Knispel <knispeja@rose-hulman.edu>
2016-10-22 18:01:04 -0400

    Added movement/collision to maze generator

--- js/mazegenerator.js
+++ js/mazegenerator.js
@@ -1,6 +1,16 @@
 const CELL_LENGTH = 10; // in px
+const TIME_PER_CELL_MS = 50;
 const USER_COLOR = "red";
+const OBSTACLE_CELL = "obstacle";
+const EMPTY_CELL = "empty";
+const OBJECTIVE_CELL = "objective";
 
+var up;
+var down;
+var left;
+var right;
+
+var animating = false;
 var maze = [];
 var userLocation = {x:0, y:0};
 var cols;
@@ -12,11 +22,14 @@ function numericOnly() {
 }
 
 function Cell(type, x, y, color) {
-
     this.type = type;
     this.x = x;
     this.y = y;
     this.color = color;
+
+    this.isObjective = function() {return this.type == OBJECTIVE_CELL;}
+    this.isObstacle = function() {return this.type == OBSTACLE_CELL;}
+    this.isEmpty = function() {return this.type == EMPTY_CELL;}
 
     this.draw = function(ctx, drawColor = this.color) {
         ctx.fillStyle = drawColor;
@@ -24,8 +37,9 @@ function Cell(type, x, y, color) {
     }
 }
 
-function makeEmptyCell(x, y) {return new Cell("empty", x, y, "white")}
-function makeObstacleCell(x, y) {return new Cell("obstacle", x, y, "black")}
+function makeObstacleCell(x, y) {return new Cell(OBSTACLE_CELL, x, y, "black");}
+function makeObjectiveCell(x, y) {return new Cell(OBJECTIVE_CELL, x, y, "green");}
+function makeEmptyCell(x, y) {return new Cell(EMPTY_CELL, x, y, "white");}
 
 function updateCanvasSize(canvas) {
     canvas.width = CELL_LENGTH * cols;
@@ -33,12 +47,11 @@ function updateCanvasSize(canvas) {
 }
 
 function generateMaze() {
-
     var newMaze = [];
-
     for(row=0; row<rows; row++) {
         var newRow = [];
         for(col=0; col<cols; col++) {
+            // TODO: Replace with real generation
             if((row + col) % 3 == 0)
                 newRow.push(makeObstacleCell(col, row));
             else
@@ -46,7 +59,6 @@ function generateMaze() {
         }
         newMaze.push(newRow);
     }
-
     return newMaze;
 }
 
@@ -62,7 +74,48 @@ function updateDrawnPlayerPosition(ctx) {
     maze[userLocation.y][userLocation.x].draw(ctx, USER_COLOR);
 }
 
+function clearDrawnPlayerPosition(ctx) {
+    maze[userLocation.y][userLocation.x].draw(ctx);
+}
+
+function reactToUserInput() {
+
+    if(animating) return;
+    if(!((up && down) || (left && right))) {
+        animating = true;
+
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+
+        clearDrawnPlayerPosition(ctx);
+
+        var newCell;
+        if(up && userLocation.y != 0) newCell = maze[userLocation.y - 1][userLocation.x];
+        else if(down && userLocation.y != cols - 1) newCell = maze[userLocation.y + 1][userLocation.x];
+
+        if(newCell && !newCell.isObstacle()) userLocation = {x: newCell.x, y: newCell.y};
+        else {
+            if(left) newCell = maze[userLocation.y][userLocation.x - 1];
+            else if(right) newCell = maze[userLocation.y][userLocation.x + 1];
+
+            if(newCell && !newCell.isObstacle()) userLocation = {x: newCell.x, y: newCell.y};
+        }
+
+        // TOOD: Check if user reached objective
+
+        // TODO: Leave trail behind user's marker
+
+        updateDrawnPlayerPosition(ctx);
+
+        animating = false;
+    }
+
+    setTimeout(reactToUserInput, TIME_PER_CELL_MS);
+}
+
 function updateMaze() {
+
+    animating = true;
 
     cols = document.getElementById("cellWidth").value;
     rows = document.getElementById("cellHeight").value;
@@ -75,6 +128,60 @@ function updateMaze() {
     var ctx = canvas.getContext("2d");
     drawMaze(ctx);
     updateDrawnPlayerPosition(ctx);
+
+    animating = false;
+    reactToUserInput();
 }
+
+function onKeyDown(event) {
+    var keyCode = event.keyCode;
+    switch(keyCode){
+        case 38:  //up arrow
+        case 87:  //w
+            event.preventDefault();
+            up = true;
+            break;
+        case 37:  //left arrow
+        case 65:  //a
+            event.preventDefault();
+            left = true;
+            break;
+        case 40:  //down arrow
+        case 83:  //s
+            event.preventDefault();
+            down = true;
+            break;
+        case 39:  //right arrow
+        case 68:  //d
+            event.preventDefault();
+            right = true;
+            break;
+    }
+}
+
+function onKeyUp(event) {
+    var keyCode = event.keyCode;
+    switch(keyCode){
+        case 38:  //up arrow
+        case 87:  //w
+            up = false;
+            break;
+        case 37:  //left arrow
+        case 65:  //a
+            left = false;
+            break;
+        case 40:  //down arrow
+        case 83:  //s
+            down = false;
+            break;
+        case 39:  //right arrow
+        case 68:  //d
+            right = false;
+            break;
+    }
+}
+
+window.addEventListener("keydown", onKeyDown, false);
+window.addEventListener("keyup", onKeyUp, false);
 
 window.onload = updateMaze;
