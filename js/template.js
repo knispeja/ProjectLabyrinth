314928704ba654e80c3b869d3970ca3c9601498e
Emily Richardson <richarel@rose-hulman.edu>
2016-10-24 19:06:01 -0400

    made it so that the header can be placed with the script, started working on main page

---
+++ js/template.js
@@ -0,0 +1,17 @@
+document.write('\
+<div class="headerArea">\
+	<img id="logo" src="images/smollogo.png" alt="Project Labyrinth">\
+		<div id="navbar">\
+			<div class="nav" id="articles">Archived Articles</div>\
+			<div class="nav" id="userMazes">User Mazes</div>\
+			<div class="nav" id="mazeGenerator">Maze Generator</div>\
+			<div class="nav" id="login">Login</div>\
+			<div class="nav" id="search">\
+				<form>\
+					Search: <input type="text" id="searchBox" value="">\
+				</form>\
+			</div>\
+		</div>\
+       <div class="coloredStripe"></div>\
+	</div>\
+');
