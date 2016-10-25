72546152f4e0ee255506630097682b2e2d6cd7ba
Emily Richardson <richarel@rose-hulman.edu>
2016-10-25 15:13:35 -0400

    took out a print statement

--- js/searchHandler.js
+++ js/searchHandler.js
@@ -1,6 +1,5 @@
 document.getElementById('searchBox').onkeypress = function (event) {
     if (event.keyCode == 13) {
-        console.log('hit');
         window.location.assign("./searchResults.html");
         return false;
     }
