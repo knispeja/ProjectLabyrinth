fdf644ceec4ec65a6676ed1db0503fd72b1fde42
karan shishoo <shishok@rose-hulman.edu>
2016-11-06 15:01:06 -0500

    added in jquery for login

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,34 @@
 var submit = document.getElementById('submit');
 var register = document.getElementById('register');
-register.addEventListener('click', function () { window.location="addNewAccount.html"; return false;});
+register.addEventListener('click', function () { window.location = "addNewAccount.html"; return false; });
 
 var apiUrl = "https://localhost:3000/login/";
 var login;
 
 function sendlogindata(login) {
-        $.ajax({
-            url: apiUrl,
-            type: 'GET',
-            data: login,
-            dataType: 'JSON',
-            success: function (data) {
-                if (data) {
-                    window.location.href = './index.html';
-                    return false;
-                } else {
-                    console.log("Maze not Found");
-                }
-            },
-            error: function (request, status, error) {
-                console.log(error, status, request);
+    $.ajax({
+        url: apiUrl,
+        type: 'GET',
+        data: login,
+        dataType: 'JSON',
+        success: function (data) {
+            if (data) {
+                window.location.href = './index.html';
+                return false;
+            } else {
+                console.log("Maze not Found");
             }
-        });
-    }
+        },
+        error: function (request, status, error) {
+            console.log(error, status, request);
+        }
+    });
+}
+
+$("#submit").click(function () {
+    var login = {
+        email: $("#Email").val(),
+        password: $("#Password").val()
+    };
+    sendlogindata(login);
+});
